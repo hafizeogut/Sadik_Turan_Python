5aa5d87d210ec61f12339cfd322a65c8e43475c1
Hafize Öğüt <94183443+hafizeogut@users.noreply.github.com>
2023-02-18 02:30:39 +0300

    moduller_hataAyiklama

--- Python/5_Moduller/main.py
+++ Python/5_Moduller/main.py
@@ -1,3 +1,23 @@
-import mod
+ 
+def factoriyel(x):
+    x=int(x)
+    
+    if x<0:
+        raise ValueError('Negatif Değer')
+        
+    result=1
+    
+    for i in range(1,x+1):
+        result *=i
+        
+    return result
 
-help(mod)
+for x in [5,10,3,-1,'-10a']:
+
+    try:
+        y=factoriyel(x)
+        
+    except ValueError as err:
+        print(err)
+        
+print(factoriyel(y))
