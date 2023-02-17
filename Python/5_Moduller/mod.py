c4b2dd70978b7b75ad88b09d393811c3789510d4
Hafize Öğüt <94183443+hafizeogut@users.noreply.github.com>
2023-02-17 21:54:49 +0300

    Moduller

---
+++ Python/5_Moduller/mod.py
@@ -0,0 +1,57 @@
+'''
+   modül hakkında bilgilendirme
+ 
+'''
+
+print('modül eklendi')
+number=10
+
+numbers=[307,1844,571]
+def func(x):
+    '''
+        fonksiyon hakkında bilgi veriniz
+    
+    
+    '''
+    print(f'x: {x}')
+    
+    
+func(5)
+
+person = {
+    "name": "Ali",
+    "age":"25",
+    "city":"istanbul"
+}
+
+
+class Person:
+    def speak(self):
+ 
+      print('I am speaking')
+        
+'''
+   modül hakkında bilgilendirme 
+'''
+
+print('modül eklendi')
+
+number = 10
+
+numbers = [1,2,3]
+
+person = {
+    "name": "Ali",
+    "age":"25",
+    "city":"istanbul"
+}
+
+def func(x):
+    '''
+        fonksiyon hakkında bilgilendirme
+    '''
+    print(f'x: {x}')
+
+class Person:
+    def speak(self):
+        print('I am speaking...')
